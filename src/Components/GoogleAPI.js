2b9d2a84b5c33a216e664bff710d509ce7d25c3d
Kendrick Koh <kendrickkoh3@gmail.com>
2023-08-28 20:04:56 +0800

    Googleapi working

--- src/Components/GoogleAPI.js
+++ src/Components/GoogleAPI.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
 
 const GoogleAPI = () => {
     const {isLoaded} = useJsApiLoader({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-        
     });
     if(!isLoaded){
         return <h1>Loading...</h1>;
     }
 
     return(
-        <GoogleMap zoom={10} center={{lat:44, lng:-80}} mapContainerStyle={{width:'100%', height:'100%'}}>
-
+        
+        <GoogleMap zoom={13} center={{lat:1.290270, lng:103.851959}} mapContainerStyle={{width:"100%", height:'100vh'}}>
+            <Marker position={{lat:1.290270, lng:103.851959}} />
         </GoogleMap>
     ) 
 }
