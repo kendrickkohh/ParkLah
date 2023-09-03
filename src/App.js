4afe00580ac6f9d8d19e333abeff42d06b1378c3
Kendrick Koh <kendrickkoh3@gmail.com>
2023-09-03 21:31:54 +0800

    Added locationService

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import GoogleAPI from "./Components/GoogleAPI";
 import Home from "./Components/Home";
+import LocationService from "./Components/LocationService";
 
 function App() {
   const [showPage, setShowPage] = useState(0);
@@ -9,7 +10,8 @@ function App() {
     <div className="indexPage">
       <div className="indexItem">
         {showPage === 0 && <Home setShowPage={setShowPage} />}
-        {showPage === 1 && <GoogleAPI />}
+        {showPage === 1 && <LocationService setShowPage={setShowPage} />}
+        {showPage === 2 && <GoogleAPI />}
       </div>
     </div>
   );
